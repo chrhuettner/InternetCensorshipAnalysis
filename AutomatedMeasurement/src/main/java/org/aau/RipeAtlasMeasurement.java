64284581c75c6540363dfd42abbdc441ca599ab0
chrhuettner <chrhuettner@edu.aau.at>
2025-07-02 22:50:57 +0200

    feat: optimized measurement order, updated target list

--- AutomatedMeasurement/src/main/java/org/aau/RipeAtlasMeasurement.java
+++ AutomatedMeasurement/src/main/java/org/aau/RipeAtlasMeasurement.java
@@ -13,11 +13,10 @@ import org.apache.hc.core5.http.ContentType;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import org.apache.hc.client5.http.classic.methods.HttpGet;
@@ -31,13 +30,18 @@ public class RipeAtlasMeasurement {
     //set RIPE_API_KEY=<Key> in environment beforehand!
     static String apiKey = System.getenv("RIPE_API_KEY");
 
-    static String TARGET_URLS = "Top100FQDN";
+    static String TARGET_URLS = "targets";
 
     static int CONCURRENT_MEASUREMENT_LIMIT = 100;
-    static int BATCH_SIZE = 20;
+    static int CONCURRENT_MEASUREMENT_STARTER_THREADS = 50;
+
 
     static AtomicInteger activeMeasurements = new AtomicInteger(0);
     static AtomicInteger activeMeasurementThreads = new AtomicInteger(0);
+    static AtomicInteger traversalIndex = new AtomicInteger(0);
+
+    static HashMap<Integer, AtomicBoolean> targetsInActiveMeasurementMap = new HashMap<>();
+    static ConcurrentHashMap<Long, Integer> measurementIdToMeasurementIndexMap = new ConcurrentHashMap<>();
 
     // Extracted with CountryCodeFetcher
     static String[] countryCodesWithProbes = new String[]{"AD", "AE", "AF", "AL", "AM", "AO", "AR", "AT", "AU", "AX", "AZ",
@@ -49,18 +53,26 @@ public class RipeAtlasMeasurement {
             "MK", "ML", "MM", "MN", "MP", "MR", "MT", "MU", "MV", "MW", "MX", "MY", "MZ", "NA", "NC", "NG", "NI", "NL", "NO",
             "NP", "NZ", "OM", "PA", "PE", "PF", "PG", "PH", "PK", "PL", "PR", "PS", "PT", "PW", "PY", "QA", "RE", "RO", "RS",
             "RU", "RW", "SA", "SC", "SE", "SG", "SI", "SK", "SL", "SN", "SV", "TD", "TH", "TJ", "TL", "TN", "TO", "TR", "TT",
-            "TW", "TZ", "UA", "UG", "US", "UY", "UZ", "VA", "VE", "VI", "VN", "XK", "YE", "ZA", "ZM", "ZW",};
+            "TW", "TZ", "UA", "UG", "US", "UY", "UZ", "VA", "VE", "VI", "VN", "XK", "YE", "ZA", "ZM", "ZW"};
+
+    static int BATCH_SIZE = Math.min(25, countryCodesWithProbes.length);
 
     static List<Map<String, Object>> targets = new ArrayList<>();
 
+    // Ideally we would use List<int[]>, but that would make the assertMeasurementIndicesConstraint method more complicated
+    static List<List<Integer>> traversalOrder = new ArrayList<>();
+
     static {
-        System.out.println("Reading Target URLs");
+        Log.info("Reading Target URLs");
         try {
             BufferedReader br = new BufferedReader(new FileReader(TARGET_URLS));
 
             String line;
 
             for (int i = 0; (line = br.readLine()) != null; i++) {
+                if (line.startsWith("NOT_REACHABLE")) {
+                    continue;
+                }
                 Map<String, Object> definition = new HashMap<>();
                 definition.put("target", line);
                 definition.put("description", "Rank " + i);
@@ -74,13 +86,23 @@ public class RipeAtlasMeasurement {
             throw new RuntimeException(e);
         }
 
+        for (int i = 0; i < targets.size(); i++) {
+            targetsInActiveMeasurementMap.put(i, new AtomicBoolean(false));
+        }
+
+        Log.info("Computing optimal request order");
+        traversalOrder = getMeasurementIndices();
+
+        Log.info("Asserting wholeness and uniqueness of measurements in the traversal order");
+        assertMeasurementIndicesConstraint();
+        Log.info("Traversal order is valid");
     }
 
     public static Map<String, Object> generateDefinitionPayload(int startCountryIndex, int targetIndex) {
         Map<String, Object> payload = new HashMap<>();
         List<Map<String, Object>> probeList = new ArrayList<>();
 
-        for (int i = startCountryIndex; i < Math.min(countryCodesWithProbes.length, startCountryIndex+BATCH_SIZE); i++) {
+        for (int i = startCountryIndex; i < Math.min(countryCodesWithProbes.length, startCountryIndex + BATCH_SIZE); i++) {
 
             Map<String, Object> probe = new HashMap<>();
             probe.put("requested", 1);
@@ -93,11 +115,13 @@ public class RipeAtlasMeasurement {
         payload.put("definitions", List.of(targets.get(targetIndex)));
         payload.put("probes", probeList);
 
+        Log.info("Generated payload for target " + targetIndex + " and countries from index " + startCountryIndex + " until " + Math.min(countryCodesWithProbes.length, startCountryIndex + BATCH_SIZE));
+
         return payload;
     }
 
 
-    public static List<Long> createMeasurements(int countryCodeIndex, int targetIndex) {
+    public static List<Long> createMeasurements(int measurementIndex) {
         String apiKey = System.getenv("RIPE_API_KEY");
         if (apiKey == null || apiKey.isBlank()) {
             throw new IllegalStateException("RIPE_API_KEY environment variable not set.");
@@ -105,6 +129,8 @@ public class RipeAtlasMeasurement {
 
         String url = "https://atlas.ripe.net/api/v2/measurements/";
 
+        int targetIndex = traversalOrder.get(measurementIndex).get(0);
+        int countryCodeIndex = traversalOrder.get(measurementIndex).get(1);
 
         Map<String, Object> payload = generateDefinitionPayload(countryCodeIndex, targetIndex);
 
@@ -126,7 +152,7 @@ public class RipeAtlasMeasurement {
                 int status = response.getCode();
                 String responseBody = EntityUtils.toString(response.getEntity());
                 if (status != 201) {
-                    Log.error("Failed to create measurement. Target: "+targets.get(targetIndex).get("target")+" Country: "+countryCodesWithProbes[countryCodeIndex]+" Status " + status + ": " + responseBody);
+                    Log.error("Failed to create measurement. Target: " + targets.get(targetIndex).get("target") + " Country: " + countryCodesWithProbes[countryCodeIndex] + " Status " + status + ": " + responseBody);
                     activeMeasurements.decrementAndGet();
                     return new ArrayList<>();
                 }
@@ -137,11 +163,13 @@ public class RipeAtlasMeasurement {
                 for (int i = 0; i < measurements.size(); i++) {
                     long id = measurements.get(i).asLong();
 
-                    for (int j = countryCodeIndex; j < Math.min(countryCodesWithProbes.length, countryCodeIndex+BATCH_SIZE); j++) {
+                    for (int j = countryCodeIndex; j < Math.min(countryCodesWithProbes.length, countryCodeIndex + BATCH_SIZE); j++) {
+                        //TODO Fix this by dynamically accessing the ripe atlass api (now its lots of duplicates)
                         SQLiteConnector.createMeasurementContext(id, (String) targets.get(targetIndex).get("target"), countryCodesWithProbes[j]);
 
                     }
                     measurementIds.add(id);
+                    measurementIdToMeasurementIndexMap.put(id, measurementIndex);
                 }
 
                 return measurementIds;
@@ -184,6 +212,7 @@ public class RipeAtlasMeasurement {
         int elapsed = 0;
 
         CloseableHttpClient client = HttpClients.createDefault();
+        AtomicBoolean targetIsInMeasurement = targetsInActiveMeasurementMap.get(traversalOrder.get(measurementIdToMeasurementIndexMap.get(measurementId)).get(0));
 
         while (elapsed < timeoutIntMs) {
 
@@ -192,6 +221,7 @@ public class RipeAtlasMeasurement {
                 SQLiteConnector.saveFailedMeasurement(measurementId, "MEASUREMENT", status);
                 Log.info("Measurement " + measurementId + " failed.");
                 activeMeasurements.decrementAndGet();
+                targetIsInMeasurement.set(false);
                 pb.step();
                 return;
                 //TODO: Sometimes the status seems to stay a few minutes at ongoing then goes to Stopped, maybe already consume ongoing messages if results are present
@@ -211,6 +241,7 @@ public class RipeAtlasMeasurement {
                         }
                         pb.step();
                         activeMeasurements.decrementAndGet();
+                        targetIsInMeasurement.set(false);
                         return body;
                     }
                     return null;
@@ -230,14 +261,28 @@ public class RipeAtlasMeasurement {
         pb.step();
     }
 
-    public static Thread startAsynchronousMeasurementThread(int offset, ProgressBar pb, ConcurrentLinkedQueue<Long> measurementIds) {
+    public static Thread startAsynchronousMeasurementThread(ProgressBar pb, ConcurrentLinkedQueue<Long> measurementIds) {
         activeMeasurementThreads.incrementAndGet();
         return Thread.startVirtualThread(() -> {
-            for (int j = offset; j < targets.size() + offset; j+= BATCH_SIZE) {
+            Log.info("Thread " + Thread.currentThread().getName() + " started");
+
+            while (true) {
+                int measurementIndex = traversalIndex.getAndUpdate((t) -> {
+                    if (t < traversalOrder.size()) {
+                        return t + 1;
+                    }
+                    return t;
+                });
+                if (measurementIndex >= traversalOrder.size()) {
+                    break;
+                }
+
+                Log.info("Thread " + Thread.currentThread().getName() + " aquired measurement index " + measurementIndex);
 
                 while (true) {
                     int current = activeMeasurements.get();
-                    if (current >= CONCURRENT_MEASUREMENT_LIMIT/BATCH_SIZE) {
+
+                    if (current >= CONCURRENT_MEASUREMENT_LIMIT) {
                         try {
                             Thread.sleep(200);
                         } catch (InterruptedException e) {
@@ -252,34 +297,106 @@ public class RipeAtlasMeasurement {
                     }
                 }
 
-                System.out.println(activeMeasurements.get());
+                AtomicBoolean targetIsInMeasurement = targetsInActiveMeasurementMap.get(traversalOrder.get(measurementIndex).get(0));
 
-                if (j >= targets.size()) {
-                    j -= targets.size();
+
+                while (true) {
+                    if (targetIsInMeasurement.get()) {
+                        Log.info("Target of measurement " + measurementIndex + " is already in an active measurement, waiting for completion of measurement...");
+                        try {
+                            Thread.sleep(2000);
+                        } catch (InterruptedException e) {
+                            Thread.currentThread().interrupt();
+                            throw new RuntimeException(e);
+                        }
+                        continue;
+                    }
+                    if (targetIsInMeasurement.compareAndSet(false, true)) {
+                        break;
+                    }
                 }
-                measurementIds.addAll(createMeasurements(offset, j));
+
+                measurementIds.addAll(createMeasurements(measurementIndex));
                 pb.step();
             }
+
             activeMeasurementThreads.decrementAndGet();
         });
     }
 
+    public static List<List<Integer>> getMeasurementIndices() {
+        int cols = countryCodesWithProbes.length;
+        int rows = targets.size();
+        int batchesInRow = Math.ceilDiv(cols, BATCH_SIZE);
+
+        List<List<Integer>> measurementIndices = new ArrayList<>();
+
+        int batchCounter = 0;
+        int rowIterations = 0;
+
+        for (int i = 0; i < rows * batchesInRow; i++) {
+            if (i != 0 && i % rows == 0) {
+                rowIterations++;
+            }
+            if (batchCounter == (batchesInRow + rowIterations) || i % rows == 0) {
+                batchCounter = rowIterations;
+            }
+
+            List<Integer> measurementIndex = new ArrayList<>();
+            measurementIndex.add(i % rows);
+            measurementIndex.add((batchCounter % batchesInRow) * BATCH_SIZE);
+
+            //Log.info(i % rows + " " + ((batchCounter % batchesInRow) * BATCH_SIZE));
+            measurementIndices.add(measurementIndex);
+            batchCounter++;
+
+        }
+
+        return measurementIndices;
+    }
+
+    public static void assertMeasurementIndicesConstraint() {
+        int cols = countryCodesWithProbes.length;
+        int rows = targets.size();
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j += BATCH_SIZE) {
+                List<Integer> testIndices = new ArrayList<>();
+                testIndices.add(i);
+                testIndices.add(j);
+                //Log.info(i + " " + j);
+
+                if (!traversalOrder.contains(testIndices)) {
+                    System.err.println("Detected violated measurement index constraints! Measurement indices do not contain " + i + " " + j * BATCH_SIZE);
+                    System.exit(1);
+                }
+
+                if (traversalOrder.indexOf(testIndices) != traversalOrder.lastIndexOf(testIndices)) {
+                    System.err.println("Detected violated measurement index constraints! Measurement index is duplicated " + i + " " + j * BATCH_SIZE);
+                    System.exit(1);
+                }
+            }
+        }
+    }
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Starting Ripe Atlas measurement");
+        Log.info("Starting Ripe Atlas measurement");
         List<Thread> startingThreads = new ArrayList<>();
 
         ConcurrentLinkedQueue<Long> measurementIds = new ConcurrentLinkedQueue<>();
 
-        try (ProgressBar startPb = new ProgressBar("Creating one thread per country (" + countryCodesWithProbes.length + ") to produce measurements", (long) (countryCodesWithProbes.length * (double)targets.size() / (double)BATCH_SIZE));
-             ProgressBar pb = new ProgressBar("Asynchronously Waiting for measurements", measurementIds.size())) {
-            for (int i = 0; i < countryCodesWithProbes.length; i++) {
-                startingThreads.add(startAsynchronousMeasurementThread(i, startPb, measurementIds));
+        try (ProgressBar startPb = new ProgressBar("Sending measurement requests", traversalOrder.size());
+             ProgressBar pb = new ProgressBar("Receiving measurement results", measurementIds.size())) {
+
+            for (int i = 0; i < CONCURRENT_MEASUREMENT_STARTER_THREADS; i++) {
+                startingThreads.add(startAsynchronousMeasurementThread(startPb, measurementIds));
             }
 
             List<Thread> threads = new ArrayList<>();
-            outerloop: while (activeMeasurementThreads.get() > 0 || !measurementIds.isEmpty()) {
+            outerloop:
+            while (activeMeasurementThreads.get() > 0 || !measurementIds.isEmpty()) {
                 while (measurementIds.peek() == null) {
-                    if(activeMeasurementThreads.get() == 0 && measurementIds.isEmpty()) {
+                    if (activeMeasurementThreads.get() == 0 && measurementIds.isEmpty()) {
                         break outerloop;
                     }
                     Thread.sleep(200);
@@ -298,8 +415,6 @@ public class RipeAtlasMeasurement {
             for (Thread thread : startingThreads) {
                 thread.join();
             }
-
-            System.out.println("All measurements have been created.");
 
             for (Thread thread : threads) {
                 thread.join();
